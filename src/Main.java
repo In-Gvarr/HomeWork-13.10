9c29c0b4f88e70d62f257ca508473f14a4529822
In-Gvarr <112261300+In-Gvarr@users.noreply.github.com>
2022-10-16 13:15:09 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 10) {
+            a++;
+            System.out.println("I'm starting to get to grips with cycles");
+        }
+
+    }
+}
